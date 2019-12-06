09a18357d4b4461577e2ff919796e291b2f12bc7
4gaede <4gaede@informatik.uni-hamburg.de>
2019-12-06 10:52:51 -0500

    added example ServiceState to state machine

--- state_machine/smach_test.py
+++ state_machine/smach_test.py
@@ -4,6 +4,8 @@ import rospy
 import smach
 import yaml
 from os.path import dirname, abspath
+from smach_ros import ServiceState
+from beginner_tutorials.srv import AddTwoInts,AddTwoIntsResponse
 
 class SceneManager(smach.State):
     def __init__(self):
@@ -13,15 +15,16 @@ class SceneManager(smach.State):
         self.load_dialog_script()
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state FOO')
+        rospy.loginfo('Executing state SCENE')
         if self.dialog < len(self.script):
             userdata.speaker = self.script[self.dialog]["speaker"]
             userdata.audio = self.script[self.dialog]["audio"]
             self.dialog += 1
             return 'say_next_sentence'
-        elif self.scene < len(self.scenes) - 1:
+        elif self.scene < len(self.scenes):
             self.scene += 1
-            self.load_dialog_script()
+            if self.scene < len(self.scenes):
+              self.load_dialog_script()
             return 'ressource_allocation'
         else:
             return 'scenes_finished'
@@ -37,7 +40,7 @@ class Speak(smach.State):
         smach.State.__init__(self, outcomes=['speech_done'], input_keys=["speaker", "audio"])
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state BAR')
+        rospy.loginfo('Executing state SPEAK')
         rospy.loginfo(userdata.speaker)
         rospy.loginfo(userdata.audio)
         return 'speech_done'
@@ -50,19 +53,32 @@ def main():
     rospy.init_node('smach_example_state_machine')
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['experiment_ended'])
+    sm = smach.StateMachine(outcomes=['experiment_ended', 'preempted', 'aborted'])
     sm.userdata.speaker = ""
     sm.userdata.audio = ""
+    sm.userdata.a = 1
+    sm.userdata.b = 1
 
     # Open the container
     with sm:
         # Add states to the container
         smach.StateMachine.add('SCENE', SceneManager(), 
                                transitions={'say_next_sentence':'SPEAK', 
-                                            'ressource_allocation':'experiment_ended',
+                                            'ressource_allocation':'ADD_TWO_INTS',
                                             'scenes_finished':'experiment_ended'})
         smach.StateMachine.add('SPEAK', Speak(), 
                                transitions={'speech_done':'SCENE'})
+                             
+        # Callback for service response  
+        def add_two_result_cb(userdata, response):
+            rospy.loginfo("Result: %i" % response.sum)
+            return 'succeeded'
+        smach.StateMachine.add('ADD_TWO_INTS',
+                           ServiceState('/add_two_ints',
+                                        AddTwoInts,
+                                        response_cb=add_two_result_cb,
+                                        request_slots = ['a', 'b']),
+                           transitions={'succeeded':'SCENE'})
 
     # Execute SMACH plan
     outcome = sm.execute()
