f3f397d0d9962a4c8c27c8ae7de305217c105ef9
Glareh Mir <8gmir@informatik.uni-hamburg.de>
2019-12-11 16:27:12 +0100

    left handed hi motion

--- motion/my_name_movement.py
+++ motion/my_name_movement.py
@@ -2,29 +2,30 @@
 
 import time
 import rospy
-from nicomsg.msg import sff,empty
+from nicomsg.msg import sff,empty, s
 
 def move():
     pub = rospy.Publisher('/nico/motion/setAngle', sff, queue_size=10)
     dt = rospy.Publisher('/nico/motion/disableTorqueAll', empty, queue_size=10)
+    oph = rospy.Publisher('/nico/motion/openHand', s, queue_size=10)
     rospy.init_node('mover', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
     speed = 0.05
-    pub.publish("r_shoulder_y", 100, speed)
-    #pub.publish("r_shoulder_z", 50, speed)
-    pub.publish("r_arm_x", -10, speed)
-    pub.publish("r_elbow_y", -130 , speed)
-    pub.publish("r_wrist_z", -120 , speed)
+    oph.publish('RHand')
+    oph.publish('LHand')
+    pub.publish("l_shoulder_y", -100, speed)
+    pub.publish("l_arm_x", 10, speed)
+    pub.publish("l_elbow_y", 130 , speed)
+    pub.publish("l_wrist_z", 120 , speed)
     time.sleep(3)
-    pub.publish("r_shoulder_y", 0, speed)
-    pub.publish("r_shoulder_z", 0, speed)
-    pub.publish("r_arm_x", -10, speed)
-    pub.publish("r_elbow_y", 0 , speed)
-    pub.publish("r_wrist_z", -0 , speed)
+    pub.publish("l_shoulder_y", 0, speed)
+    pub.publish("l_shoulder_z", 0, speed)
+    pub.publish("l_arm_x", 10, speed)
+    pub.publish("l_elbow_y", 0 , speed)
+    pub.publish("l_wrist_z", 0 , speed)
     time.sleep(2)
     dt.publish()
     return
-
 
 if __name__ == '__main__':
     try:
