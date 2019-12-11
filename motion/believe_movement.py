0e1998ee626d61c609ec7190f3a3f9421965320d
Glareh Mir <8gmir@informatik.uni-hamburg.de>
2019-12-11 14:53:52 +0100

    Believe pose was improved

--- motion/believe_movement.py
+++ motion/believe_movement.py
@@ -2,44 +2,46 @@
 
 import time
 import rospy
-from nicomsg.msg import sff
+from nicomsg.msg import sff, empty, s
 
 
 def move():
     pub = rospy.Publisher('/nico/motion/setAngle', sff, queue_size=10)
+    dta = rospy.Publisher('/nico/motion/disableTorqueAll', empty, queue_size=10)
+    oph = rospy.Publisher('/nico/motion/openHand', s, queue_size=10)
     rospy.init_node('mover', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
+    speed=0.07
     # move
-    pub.publish("r_shoulder_y", 40, 0.3)
-    pub.publish("r_arm_x", 10, 0.3)
-    pub.publish("r_elbow_y", -100 , 0.3)
-    pub.publish("r_wrist_x", -25 , 0.3)
-    pub.publish("r_indexfingers_x", 40,.3)
-    pub.publish("r_middlefingers_x", -40., .3)
-    pub.publish("r_indexfinger_x", 40,.3)
-    pub.publish("r_thumb_z", -40, .3)
-    pub.publish("r_thumb_x", -40, .3)
-    time.sleep(5)
+    pub.publish('head_y' , 0, speed)
+    pub.publish("r_wrist_x", -25 , speed)
+    pub.publish("r_indexfinger_x", -150, speed)
+    pub.publish("r_middlefingers_x", 150, speed +.02) # upper 180 lower -150
+    pub.publish("r_thumb_z", 150, speed)
+    pub.publish("r_thumb_x", 0, speed)
+    pub.publish("r_shoulder_y", 50, speed)
+    pub.publish("r_arm_x", -15, speed)
+    pub.publish("r_elbow_y", -100 , speed)
+    time.sleep(1.5)
     # back
-    pub.publish("r_elbow_y", -90 , 0.3)
-    time.sleep(0.8)
+    pub.publish("r_elbow_y", -90 , speed)
+    time.sleep(.7)
     # forward
-    pub.publish("r_elbow_y", -100 , 0.3)
-    time.sleep(0.8)
+    pub.publish("r_elbow_y", -100 , speed)
+    time.sleep(.7)
     # back
-    pub.publish("r_elbow_y", -90 , 0.3)
-    time.sleep(0.8)
-    pub.publish("r_shoulder_y", 0, 0.1)
-    pub.publish("r_shoulder_z", 0, 0.1)
-    pub.publish("r_arm_x", 0, 0.1)
-    pub.publish("r_elbow_y", 0 , 0.1)
-    pub.publish("r_wrist_x", -0 , 0.1)
-    pub.publish("r_indexfingers_x", 0,.1)
-    pub.publish("r_middlefingers_x", 0., .1)
-    pub.publish("r_indexfinger_x", 0,.1)
-    pub.publish("r_thumb_z", 0, .1)
-    pub.publish("r_thumb_x", 0, .1)
+    pub.publish("r_elbow_y", -90 , speed)
+    time.sleep(.7)
+    pub.publish("r_shoulder_y", 0, speed)
+    pub.publish("r_shoulder_z", 0, speed)
+    pub.publish("r_arm_x", -15, speed)
+    pub.publish("r_elbow_y", 0 , speed)
+    pub.publish("r_wrist_x", -0 , speed)
     time.sleep(1)
+    oph.publish('RHand')
+    oph.publish('LHand')
+    time.sleep(1)
+    dta.publish()
     return
 
 
