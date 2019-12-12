844e345145de783cb8ab5cd8b5d81ba25bb961aa
Christoper Wulur <8wulur@informatik.uni-hamburg.de>
2019-12-12 12:08:39 +0100

    set face to happy when raise hand

--- motion/my_name_movement.py
+++ motion/my_name_movement.py
@@ -8,11 +8,13 @@ def move():
     pub = rospy.Publisher('/nico/motion/setAngle', sff, queue_size=10)
     dt = rospy.Publisher('/nico/motion/disableTorqueAll', empty, queue_size=10)
     oph = rospy.Publisher('/nico/motion/openHand', s, queue_size=10)
+    face = rospy.Publisher('/nico/faceExpression', s, queue_size=10)
     rospy.init_node('mover', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
     speed = 0.05
     oph.publish('RHand')
     oph.publish('LHand')
+    face.publish("happiness")
     pub.publish("l_shoulder_y", -100, speed)
     pub.publish("l_arm_x", 10, speed)
     pub.publish("l_elbow_y", 130 , speed)
@@ -23,6 +25,7 @@ def move():
     pub.publish("l_arm_x", 10, speed)
     pub.publish("l_elbow_y", 0 , speed)
     pub.publish("l_wrist_z", 0 , speed)
+    face.publish("neutral")
     time.sleep(2)
     dt.publish()
     return
