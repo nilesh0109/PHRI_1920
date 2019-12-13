c29e2cef8fb3ffd39ab6d21b5e942c5d5e49f268
4gaede <4gaede@informatik.uni-hamburg.de>
2019-12-13 17:31:07 +0100

    added question answer related states to state machine

---
+++ state_machine/StateMachine.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import yaml
+from os.path import dirname, abspath
+from smach_ros import ServiceState
+from beginner_tutorials.srv import SpeechRecognition,SpeechRecognitionResponse
+
+class SceneFlow(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['say_robot_line', 'say_ship_line','participant_input','ressource_allocation','scenes_finished'], output_keys=["speaker", "audio", "scene"])
+        self.scene_index = 0
+        self.scenes = ["scene1",]
+        self.load_dialog_script()
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state SCENE')
+        if self.dialog < len(self.script):
+            speaker = self.script[self.dialog]["speaker"]
+            if speaker == "p":
+              self.dialog += 1
+              return 'participant_input'
+            else :
+              userdata.speaker = speaker
+              userdata.audio = self.script[self.dialog]["audio"]
+              self.dialog += 1
+              if speaker == "s":
+                return 'say_ship_line'
+              else:
+                return 'say_robot_line'
+        elif self.scene_index < len(self.scenes):
+            self.scene_index += 1
+            if self.scene_index < len(self.scenes):
+              userdata.scene = self.scenes[scene_index]
+              self.load_dialog_script()
+            return 'ressource_allocation'
+        else:
+            return 'scenes_finished'
+
+    def load_dialog_script(self):
+      self.dialog = 0
+      with open(dirname(abspath(__file__)) + "/" + self.scenes[self.scene_index] + ".yml", "r") as f:
+        self.script = yaml.safe_load(f)
+        
+
+class Speak(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['speech_done'], input_keys=["speaker", "audio"])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state SPEAK')
+        rospy.loginfo(userdata.speaker)
+        rospy.loginfo(userdata.audio)
+        return 'speech_done'
+
+class SayResponses(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['say_robot_line','say_ship_line','participant_input','questions_done'], input_keys=["question", "scene"], output_keys=["speaker", "audio"])
+        self.repeat_once = False
+        self.answer_index = 0
+        with open(dirname(abspath(__file__)) + "/questions.yml", "r") as f:
+          self.question_answers = yaml.safe_load(f)
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state RESOLVE_QUESTION')
+        answers = self.question_answers[userdata.question]
+        if userdata.scene in answers:
+          answers = answers[userdata.scene]
+        else:
+          self.repeat_once = True
+        if self.answer_index < len(answers):
+            answer = answers[self.answer_index]
+            userdata.speaker = answer["speaker"]
+            userdata.audio = answer["audio"]
+            self.answer_index += 1
+            if answer["speaker"] == "s":
+              return 'say_ship_line'
+            else:
+              return 'say_robot_line'
+        else:
+            self.answer_index = 0
+            if self.repeat_once:
+              self.repeat_once = False
+              return 'participant_input'
+            else:
+              return 'questions_done'
+      
+
+# main
+def main():
+    rospy.init_node('smach_example_state_machine')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['experiment_ended', 'preempted', 'aborted'])
+    sm.userdata.speaker = ""
+    sm.userdata.audio = ""
+    sm.userdata.scene = "scene1"
+    sm.userdata.question = ""
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('SCENE', SceneFlow(), 
+                               transitions={'say_robot_line':'SPEAK', 
+                                            'say_ship_line':'SPEAK',
+                                            'participant_input':'GET_QUESTION',
+                                            'ressource_allocation': 'experiment_ended',
+                                            'scenes_finished':'experiment_ended'})
+        smach.StateMachine.add('SPEAK', Speak(), 
+                               transitions={'speech_done':'SCENE'})
+                               
+        smach.StateMachine.add('SPEAK2', Speak(), 
+                               transitions={'speech_done':'RESOLVE_QUESTION'})
+        
+        smach.StateMachine.add('GET_QUESTION',
+                           ServiceState('/question_recognition',
+                                        SpeechRecognition,
+                                        response_slots = ['question']),
+                           transitions={'succeeded':'RESOLVE_QUESTION'})
+        
+        smach.StateMachine.add('RESOLVE_QUESTION', SayResponses(), 
+                               transitions={'say_robot_line':'SPEAK2',
+                                            'say_ship_line':'SPEAK2',
+                                            'participant_input':'GET_QUESTION',
+                                            'questions_done':'SCENE'})
+                             
+        # Callback for service response  
+        #def add_two_result_cb(userdata, response):
+        #    rospy.loginfo("Result: %i" % response.sum)
+        #    return 'succeeded'
+        
+        # ServiceState(.., response_cb=add_two_result_cb, ..)
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+
+
+if __name__ == '__main__':
+    main()
+
