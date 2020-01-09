cd8e7a75dab33215853514e20007ddcc7e9b132d
8gmir <glareh.mir@studium.uni-hamburg.de>
2020-01-09 17:38:16 +0100

    nicopose cleaned up

--- nicopose/src/pose.py
+++ nicopose/src/pose.py
@@ -4,7 +4,6 @@
 import rospy
 from nicopose.srv import Pose, PoseResponse
 from nicomotion import Mover, Motion
-import poses
 
 class Move():
     pos = None
@@ -13,11 +12,11 @@ class Move():
     mov = None
     mover_path = "../../../../moves_and_positions/"
     def __init__(self):
-        self.pos = poses.Poses()
         self.robot = Motion.Motion('../../../../json/nico_humanoid_upper_rh7d.json', vrep=False)
         self.mov = Mover.Mover(self.robot, stiff_off=True)
         self.function_dict = {
             'hi': 'hi.csv',
+            'rand' : 'pos_head_look_down.csv',
         }
         return
 
@@ -27,12 +26,19 @@ class Move():
         if (p.pose_name in self.function_dict):
             fname = self.mover_path + self.function_dict[p.pose_name]
             self.mov.play_movement(fname, move_speed=0.04)
-            self.robot.disableTorqueAll()
+            self.relax()
             res.msgback = 1
         else:
             print("Pose not found.")
             res.msgback = 0
         return res
+
+    def relax(self):
+        self.robot.disableTorqueAll()
+        self.robot.openHand('RHand')
+        self.robot.openHand('LHand')
+        # Maybe head comes to center?
+        return
 
 
 if __name__ == "__main__":
