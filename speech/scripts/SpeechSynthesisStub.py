bc2679dd07e9a26497c3d6ff115545df966ed5da
Sebastian Zoerner <zoerner7@gmail.com>
2020-01-09 15:16:14 +0100

    Play generated files.

--- speech/scripts/SpeechSynthesisStub.py
+++ speech/scripts/SpeechSynthesisStub.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 
 from speech.srv import SpeechSynthesis, SpeechSynthesisResponse
+from nicoaudio.AudioPlayer import AudioPlayer
 import random
 import rospy
 
-def handle_synthesis_request(req):
+sound_dict = {"hello_commander": "scene_0_line_0_S.mp3",
+			  "systems_clear":	 "scene_0_line_1_S.mp3"}
 
-    print "Sentence {} done.".format(req.sentence)
-    return SpeechSynthesisResponse(True)
+def play_audio(file_name, sounds_dir="generated_sounds/"):
+	# load file
+	playback = AudioPlayer(sounds_dir + file_name)
+	# start playback
+	playback.play()
+	# wait until playback is finished
+	time.sleep(playback.duration - playback.position)
+
+def handle_synthesis_request(req):
+	file_name = sound_dict.get(req.sentence)
+	play_audio(file_name)
+	print "Playing \"{}\" done.".format(req.sentence)
+	return SpeechSynthesisResponse(True)
+
 
 def speech_synthesis_server():
     rospy.init_node('speech_synthesis_server')
