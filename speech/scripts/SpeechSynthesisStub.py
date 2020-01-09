b6a9081e60de26b4e42ab25c104bb261d0a44e2f
Sebastian Zoerner <zoerner7@gmail.com>
2020-01-09 15:29:12 +0100

    Correct formatting to spaces.

--- speech/scripts/SpeechSynthesisStub.py
+++ speech/scripts/SpeechSynthesisStub.py
@@ -6,21 +6,21 @@ import random
 import rospy
 
 sound_dict = {"hello_commander": "scene_0_line_0_S.mp3",
-			  "systems_clear":	 "scene_0_line_1_S.mp3"}
+              "systems_clear":   "scene_0_line_1_S.mp3"}
 
 def play_audio(file_name, sounds_dir="generated_sounds/"):
-	# load file
-	playback = AudioPlayer(sounds_dir + file_name)
-	# start playback
-	playback.play()
-	# wait until playback is finished
-	time.sleep(playback.duration - playback.position)
+    # load file
+    playback = AudioPlayer(sounds_dir + file_name)
+    # start playback
+    playback.play()
+    # wait until playback is finished
+    time.sleep(playback.duration - playback.position)
 
 def handle_synthesis_request(req):
-	file_name = sound_dict.get(req.sentence)
-	play_audio(file_name)
-	print "Playing \"{}\" done.".format(req.sentence)
-	return SpeechSynthesisResponse(True)
+    file_name = sound_dict.get(req.sentence)
+    play_audio(file_name)
+    print "Playing \"{}\" done.".format(req.sentence)
+    return SpeechSynthesisResponse(True)
 
 
 def speech_synthesis_server():
