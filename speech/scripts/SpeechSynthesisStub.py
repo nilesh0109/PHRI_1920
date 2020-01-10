341bc2874a84b165ac6ad91abb63b29d8e7e48e6
Sebastian Zoerner <zoerner7@gmail.com>
2020-01-10 16:35:02 +0100

    Fetch file name from service parameter.

--- speech/scripts/SpeechSynthesisStub.py
+++ speech/scripts/SpeechSynthesisStub.py
@@ -6,16 +6,12 @@ import rospy
 from AudioPlayer import AudioPlayer
 from os.path import dirname, abspath
 
-sound_dict = {"hello_commander": "scene_0_line_0_S.mp3",
-              "systems_clear":   "scene_0_line_1_S.mp3"}
-
 def handle_synthesis_request(req):
-    print 'Sentence {} done.'.format(req.sentence)
     player = AudioPlayer()
-    filepath = dirname(dirname(abspath(__file__))) + '/generated_sounds/sample.wav'
+    filepath = dirname(dirname(abspath(__file__))) + '/generated_sounds/{}.wav'.format(req.sentence)
     player.play(filepath)
+    print 'Sentence {} done.'.format(req.sentence)
     return SpeechSynthesisResponse(True)
-
 
 def speech_synthesis_server():
     rospy.init_node('speech_synthesis_server')
