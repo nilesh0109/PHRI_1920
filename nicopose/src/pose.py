abf43758e4db8c105e3aa348c3171d9a8ab8e3ae
Christoper Wulur <8wulur@informatik.uni-hamburg.de>
2020-01-10 10:35:29 -0500

    multithreading for motion, utterance and expression

--- nicopose/src/pose.py
+++ nicopose/src/pose.py
@@ -1,36 +1,59 @@
 #!/usr/bin/env python
 # DO NOT REMOVE THE ABOVE!!!
+import time
 
 import rospy
 from nicopose.srv import Pose, PoseResponse
 from nicomotion import Mover, Motion
+import json
+import glob
+import re
+
+import threading
+
+TYPE_MOVEMENT = "MOVEMENT"
+TYPE_UTTERANCE = "UTTERANCE"
+TYPE_EXPRESSION = "EXPRESSION"
+
+
+def fetch_poses():
+    poses = {}
+
+    for filename in glob.glob("../../../../poses/*.json"):
+        with open(filename, 'r') as f:
+            name = re.search(r"^.*/(.*).json$", filename).group(1)
+            poses[name] = json.load(f)
+
+    return poses
 
 class Move():
-    pos = None
-    function_dict = None
-    robot = None
-    mov = None
-    mover_path = "../../../../moves_and_positions/"
+
     def __init__(self):
         self.robot = Motion.Motion('../../../../json/nico_humanoid_upper_rh7d.json', vrep=False)
         self.mov = Mover.Mover(self.robot, stiff_off=True)
-        self.function_dict = {
-            'hi': 'hi.csv',
-            'rand' : 'pos_head_look_down.csv',
-        }
+        self.poses = fetch_poses()
         return
 
+
+
     def response(self, p):
-        print(p.pose_name)
         res = PoseResponse()
-        if (p.pose_name in self.function_dict):
-            fname = self.mover_path + self.function_dict[p.pose_name]
-            self.mov.play_movement(fname, move_speed=0.04)
-            self.relax()
-            res.msgback = 1
-        else:
+        print(p.pose_name)
+
+        tasks = self.poses.get(p.pose_name, None)
+        if tasks is None:
             print("Pose not found.")
             res.msgback = 0
+            return res
+
+        for task in tasks:
+            thread = threading.Thread(target=self.task_job, args=(task,))
+            thread.start()
+            thread.join()
+
+        # self.relax()
+        res.msgback = 1
+
         return res
 
     def relax(self):
@@ -38,6 +61,34 @@ class Move():
         self.robot.openHand('RHand')
         self.robot.openHand('LHand')
         # Maybe head comes to center?
+        return
+
+    def task_job(self, task):
+        time.sleep(task["time"])
+
+        if task["type"] == TYPE_MOVEMENT:
+            print("EXECUTING MOVEMENT {} at time {} ----- ".format(task["value"], task["time"]))
+            self.play_movement(task["value"])
+
+            return
+        elif task["type"] == TYPE_EXPRESSION:
+            print("EXECUTING EXPRESSION {} at time {} ----- ".format(task["value"], task["time"]))
+            return
+        elif task["type"] == TYPE_UTTERANCE:
+            print("EXECUTING UTTERANCE {} at time {} ----- ".format(task["value"], task["time"]))
+        return
+
+    def play_movement(self, movement_name):
+        self.mov.play_movement(movement_name, move_speed=0.01)
+        self.relax()
+        return
+
+    def play_utterance(self, utterance_name):
+        # TODO: IMPLEMENTATION OF UTTERANCE
+        return
+
+    def play_expression(self, expression_name):
+        # TODO: IMPLEMENTATION OF EXPRESSION
         return
 
 
