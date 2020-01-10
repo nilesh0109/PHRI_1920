38dffba2c04ff8e2e56aea78e72e91fad7709e08
Sebastian Zoerner <zoerner7@gmail.com>
2020-01-10 17:17:31 +0100

    Change naming scheme to scene_#_speaker_#_line_#.

--- speech/prototype/tts_converter.py
+++ speech/prototype/tts_converter.py
@@ -13,6 +13,8 @@ polly_client = boto3.Session().client('polly')
 
 # %% main
 for scene_idx, scene in enumerate(script):
+    # set the initial speaker index for every speaker to 0
+    speaker_idx = {k: 0 for k in speakers.keys()}
     for line_idx, line in enumerate(scene['scene']):
         name = line["speaker"]
         voice = speakers[name]['voice']
@@ -22,10 +24,12 @@ for scene_idx, scene in enumerate(script):
             response = polly_client.synthesize_speech(Text=line["text"], VoiceId=voice, TextType='ssml',
                                                       Engine=engine, OutputFormat='pcm')
             # write PCM data to a .wav file
-            file_path = f'generated_sounds/scene_{scene_idx}_line_{line_idx}_{name}.wav'
+            file_path = f'generated_sounds/scene_{scene_idx}_{name}_line_{speaker_idx[name]}.wav'
             with wave.open(file_path, 'wb') as wav_file:
                 wav_file.setparams((1, 2, 16000, 0, 'NONE', 'NONE'))
                 wav_file.writeframes(response['AudioStream'].read())
+            # increment the speaker index
+            speaker_idx[name] += 1
             print(f'Processed Scene {scene_idx}, Line {line_idx}.')
         except Exception as e:
             print(f'Error in Scene {scene_idx}, Line {line_idx}:'
