4256704ddded9511f81ed4c3eb035193df4188e9
4gaede <4gaede@informatik.uni-hamburg.de>
2020-01-17 13:55:15 +0100

    implemented allocation (SCENE->WAIT_FOR_CUBES->CUBE_COUNT->SCENE)

--- state_machine/scripts/StateMachine.py
+++ state_machine/scripts/StateMachine.py
@@ -4,13 +4,20 @@ import argparse
 import rospy
 import smach
 from smach_ros import ServiceState
-from state_machine.srv import SpeechRecognition
+from speech.srv import SpeechRecognition, SpeechRecognitionRequest
+from vision.srv import CountResources
 from states import SceneFlow, SayResponses, Speak
 
 # main
 def main():
 
     parser = argparse.ArgumentParser(description="Experiment state machine.")
+    parser.add_argument(
+        "--scene_file",
+        type=str,
+        default="scene.yml",
+        help="YAML file which contains the event order",
+    )
     parser.add_argument("--scene", type=int, default=0, help="Index of the start scene")
     parser.add_argument(
         "--entry", type=int, default=0, help="Script entry in scene to start at"
@@ -25,21 +32,23 @@ def main():
 
     sm.userdata.speaker = ""
     sm.userdata.audio = ""
-    sm.userdata.scene = "scene1"
-    sm.userdata.question = ""
+    sm.userdata.scene = "scene_0"  # TODO adjust assignment for dynamic scene entry
+    sm.userdata.sentence = ""
     sm.userdata.qa_once = False
+    sm.userdata.no_of_objects = 69420
 
     # Open the container
     with sm:
         # Add states to the container
         smach.StateMachine.add(
             "SCENE",
-            SceneFlow.SceneFlow(),
+            SceneFlow.SceneFlow(args.scene_file, args.scene, args.entry),
             transitions={
                 "say_robot_line": "SPEAK",
                 "say_ship_line": "SPEAK",
                 "participant_input": "GET_QUESTION",
-                "ressource_allocation": "SCENE",
+                "ressource_allocation": "WAIT_FOR_CUBES",
+                "unknown_event": "aborted",
                 "scenes_finished": "experiment_ended",
             },
         )
@@ -70,6 +79,24 @@ def main():
             },
         )
 
+        smach.StateMachine.add(
+            "WAIT_FOR_CUBES",
+            ServiceState(
+                "/speech_recognition",
+                SpeechRecognition,
+                request=SpeechRecognitionRequest("done"),
+                response_slots=["sentence"],
+            ),
+            transitions={"succeeded": "CUBE_COUNT"},
+        )
+
+        smach.StateMachine.add(
+            "CUBE_COUNT",
+            ServiceState(
+                "/count_objects", CountResources, response_slots=["no_of_objects"]
+            ),
+            transitions={"succeeded": "SCENE"},
+        )
         # Callback for service response
         # def add_two_result_cb(userdata, response):
         #    rospy.loginfo("Result: %i" % response.sum)
@@ -79,6 +106,10 @@ def main():
 
     # Execute SMACH plan
     outcome = sm.execute()
+
+    rospy.loginfo("nof_cubes = %i", sm.userdata.no_of_objects)
+    rospy.loginfo("recognized sentence = %s", sm.userdata.sentence)
+    rospy.loginfo("StateMachine ended with outcome %s", outcome)
 
 
 if __name__ == "__main__":
--- state_machine/scripts/states/SceneFlow.py
+++ state_machine/scripts/states/SceneFlow.py
@@ -5,7 +5,7 @@ import yaml
 
 
 class SceneFlow(smach.State):
-    def __init__(self):
+    def __init__(self, scene_file="scene.yml", scene=0, dialog=0):
         smach.State.__init__(
             self,
             outcomes=[
@@ -13,54 +13,53 @@ class SceneFlow(smach.State):
                 "say_ship_line",
                 "participant_input",
                 "ressource_allocation",
+                "unknown_event",
                 "scenes_finished",
             ],
             output_keys=["speaker", "audio", "scene", "qa_once"],
         )
-        self.scene_index = 0
-        self.load_dialog_script()
+        self.scene_index = scene
+        self.dialog = dialog
+        self.load_dialog_script(scene_file)
         self.scenes = sorted(self.script.keys())
 
     def execute(self, userdata):
         rospy.loginfo("Executing state SCENE")
-        if self.dialog < len(self.script[self.scenes[self.scene_index]]):
-            if (
-                self.script[self.scenes[self.scene_index]][self.dialog]["event"]
-                == "utterance"
-            ):
-                # print(self.script[self.scenes[self.scene_index]][self.dialog]["parameters"]["speaker"])
-                speaker = self.script[self.scenes[self.scene_index]][self.dialog][
-                    "speaker"
-                ]
-                if speaker == "p":
-                    userdata.qa_once = self.script[self.scenes[self.scene_index]][
-                        self.dialog
-                    ]["once"]
-                    self.dialog += 1
-                    return "participant_input"
+        if self.dialog == len(self.script[self.scenes[self.scene_index]]):
+            if self.scene_index < (len(self.scenes) - 1):
+                self.scene_index += 1
+                self.dialog = 0
+                if self.scene_index < len(self.scenes):
+                    userdata.scene = self.scenes[self.scene_index]
+            else:
+                return "scenes_finished"
+        event = self.script[self.scenes[self.scene_index]][self.dialog]["event"]
+        outcome = "unknown_event"
+        if event == "utterance":
+            # print(self.script[self.scenes[self.scene_index]][self.dialog]["parameters"]["speaker"])
+            speaker = self.script[self.scenes[self.scene_index]][self.dialog]["speaker"]
+            if speaker == "p":
+                userdata.qa_once = self.script[self.scenes[self.scene_index]][
+                    self.dialog
+                ]["once"]
+                self.dialog += 1
+                return "participant_input"
+            else:
+                userdata.speaker = speaker
+                userdata.audio = self.script[self.scenes[self.scene_index]][
+                    self.dialog
+                ]["audio"]
+                if speaker == "s":
+                    outcome = "say_ship_line"
                 else:
-                    userdata.speaker = speaker
-                    userdata.audio = self.script[self.scenes[self.scene_index]][
-                        self.dialog
-                    ]["audio"]
-                    self.dialog += 1
-                    if speaker == "s":
-                        return "say_ship_line"
-                    else:
-                        return "say_robot_line"
-            else:  # event != "utterance"
-                pass
-        elif self.scene_index < (len(self.scenes) - 1):
-            self.scene_index += 1
-            self.dialog = 0
-            if self.scene_index < len(self.scenes):
-                userdata.scene = self.scenes[self.scene_index]
-                # self.load_dialog_script()
-            return "ressource_allocation"
-        else:
-            return "scenes_finished"
+                    outcome = "say_robot_line"
+        elif event == "allocation":
+            outcome = "ressource_allocation"
+        else:  # unknown event
+            rospy.loginfo("Encountered unknown event %s", event)
+        self.dialog += 1
+        return outcome
 
-    def load_dialog_script(self):
-        self.dialog = 0
-        with open(dirname(abspath(__file__)) + "/scene.yml", "r") as f:
+    def load_dialog_script(self, scene_file):
+        with open(dirname(abspath(__file__)) + "/" + scene_file, "r") as f:
             self.script = yaml.safe_load(f)
