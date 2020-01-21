d7821b0962e95c11d001c0ec2d79ab006e485077
8bhatia <kavish.bhatia@studium.uni-hamburg.de>
2020-01-21 15:55:06 +0100

    added working empty table and counting cubes

--- vision/scripts/count_resources.py
+++ vision/scripts/count_resources.py
@@ -8,26 +8,32 @@ import time
 import numpy as np
 
 def handle_resources_request(participant_num):
-    #num_of_objects = 0
-    success = True
-    #success = cube_detect(participant_num)
-    if success:
-        return CountResourcesResponse(success)
-
+    success = False
+    while success == False:
+        success = cube_detect(participant_num)
+        
+        if success:
+            return CountResourcesResponse(success)
+            
+        
 
 def check_table_empty():
     table1 = []
     table2 = []
-#    while True:
-#        object1, object2 = cube_detect()
-#        table1.append(object1)
-#        table2.append(object2)
-#        time.sleep(0.2)
-#        
-#        if len(table1) >= 3:
-#            if np.average(table1[-3:]) == 0 and np.average(table2[-3:]) == 0:
-#                break
-#            return CountResourcesResponse(True)
+    start = time.time()
+    while True:
+        object1, object2 = cube_detect()
+        table1.append(object1)
+        table2.append(object2)
+        time.sleep(0.2)
+        
+        if len(table1) > 3:
+            if np.average(table1[-3:]) == 0 and np.average(table2[-3:]) == 0:
+                break
+            
+        if time.time() - start > 30: #30 seconds timeout
+            break
+        
     return CountResourcesResponse(True)
     
 
@@ -35,7 +41,7 @@ def count_resources_server(robot_name=''):
     rospy.init_node('count_resources_allocated')
     s = rospy.Service(robot_name+'/count_objects', CountResources, handle_resources_request)
     e = rospy.Service(robot_name+'/check_empty', CountResources, check_table_empty)
-    #print s
+
     rospy.spin()
 
 
