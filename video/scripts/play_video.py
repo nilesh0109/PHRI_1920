4c132a167d789a43617b04e02aded2aa3fbae325
lakomkin <lakomkin@informatik.uni-hamburg.de>
2020-01-23 18:30:27 +0100

    gitingore got mp4 exclusion and fixed video server is almost working. pause still broken

--- video/scripts/play_video.py
+++ video/scripts/play_video.py
@@ -6,17 +6,16 @@ import time
 import os
 import rospy
 from pynput.keyboard import Key, Controller
+from os.path import dirname, abspath
 
-exitFlag = 0
-cliplength = [0, 30, 30, 30, 30]
-counter = 0
+
 
 
 class myVideoThread(threading.Thread):
     def __init__(self, threadID, filename):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.filename = filename
+        self.filename = dirname(abspath(__file__)) + '/hrivid.mp4'
 
     def run(self):
         startvideo(self.filename)
@@ -47,17 +46,25 @@ def pausevideoafter(keyboard, seconds):
 
 def handle_video_request(req):
     thread2 = myPauseThread(2, cliplength[req.scene_number])
-    counter += 1
+    time.sleep(cliplength[req.scene_number])
+    #counter += 1
+    return True
 
 
 def video_server():
     rospy.init_node("play_video_server")
     s = rospy.Service("play_video", PlayVideo, handle_video_request)
-    thread1 = myVideoThread(1, "filename.mp4")
+    thread1 = myVideoThread(1, dirname(abspath(__file__)) + '/hrivid.mp4')
     thread2 = myPauseThread(2, 0)
     print("Video service launched")
+    thread1.start()
+    thread2.start()
     rospy.spin()
 
 
 if __name__ == "__main__":
+    exitFlag = 0
+    cliplength = [0, 36, 42, 49, 41, 49]
+    #counter = 0
     video_server()
+
