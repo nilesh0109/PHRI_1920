5e1c2d4e74d670408630453a5f678b2ca4cd48af
Nilesh Vijayrania <8vijayra@informatik.uni-hamburg.de>
2020-02-14 16:55:50 +0100

    removing None from feedback

--- speech/scripts/CubesCountingFeedback.py
+++ speech/scripts/CubesCountingFeedback.py
@@ -11,7 +11,7 @@ def get_audio_filename(scene_id, cubesA, cubesB):
     rospy.loginfo("scene_id={} cubesA={} cubesB={}".format(scene_id,cubesA, cubesB))
     print('scene_id={} cubesA={} cubesB={}'.format(scene_id,cubesA, cubesB))
 
-    STRONG_THRESHOLD = 4
+    STRONG_THRESHOLD = 5
     if  0<= scene_id < 5:
         filename = 'feedback_scene_'+str(scene_id)
         if cubesA >= STRONG_THRESHOLD:
@@ -23,13 +23,14 @@ def get_audio_filename(scene_id, cubesA, cubesB):
         elif cubesA < cubesB:
             filename += '_win_B'
         elif cubesA == cubesB:
-            filename += '_none' if cubesA==0 else '_1_equal'
+            filename += '_equal'
         return filename
 
 def handle_cube_counting(req):
-    speaker = 'A'
+    speaker = 'S'
     player = AudioPlayer(speaker)
     filename = get_audio_filename(req.scene_id, req.num_cubes_A, req.num_cubes_B)
+    rospy.loginfo('feedback is {:s}'.format(filename))
     if filename:
         filepath = dirname(dirname(abspath(__file__))) + '/generated_sounds/{}.wav'.format(filename)
         player.play(filepath, filename)
