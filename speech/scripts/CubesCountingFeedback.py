c14c20dfa6654582966075a55b2f5192dd83d3b2
Nilesh Vijayrania <8vijayra@informatik.uni-hamburg.de>
2020-02-14 16:41:35 +0100

    Adding the cubes counting feedback

---
+++ speech/scripts/CubesCountingFeedback.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys
+from os.path import dirname, abspath
+
+import rospy
+from speech.srv import CubeCounting, CubeCountingResponse
+from AudioPlayer import AudioPlayer
+
+def get_audio_filename(scene_id, cubesA, cubesB):
+
+    rospy.loginfo("scene_id={} cubesA={} cubesB={}".format(scene_id,cubesA, cubesB))
+    print('scene_id={} cubesA={} cubesB={}'.format(scene_id,cubesA, cubesB))
+
+    STRONG_THRESHOLD = 4
+    if  0<= scene_id < 5:
+        filename = 'feedback_scene_'+str(scene_id)
+        if cubesA >= STRONG_THRESHOLD:
+            filename += '_strong_A'
+        elif cubesB >= STRONG_THRESHOLD:
+            filename += '_strong_B'
+        elif cubesA > cubesB:
+            filename += '_win_A'
+        elif cubesA < cubesB:
+            filename += '_win_B'
+        elif cubesA == cubesB:
+            filename += '_none' if cubesA==0 else '_1_equal'
+        return filename
+
+def handle_cube_counting(req):
+    speaker = 'A'
+    player = AudioPlayer(speaker)
+    filename = get_audio_filename(req.scene_id, req.num_cubes_A, req.num_cubes_B)
+    if filename:
+        filepath = dirname(dirname(abspath(__file__))) + '/generated_sounds/{}.wav'.format(filename)
+        player.play(filepath, filename)
+        rospy.loginfo("Finished feedback")
+    return CubeCountingResponse(True)
+
+def cube_counting_server():
+    rospy.init_node('cube_counting_server', anonymous=True)
+    rospy.Service('cube_counting', CubeCounting, handle_cube_counting)
+    rospy.loginfo("Cube Counting launched.")
+    rospy.spin()
+
+if __name__ == "__main__":
+    cube_counting_server()
